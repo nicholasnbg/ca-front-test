0384d3f9f8abc9d4ac97ef5aee518cfbeccad053
Nick Gray <nicholasnbg@gmail.com>
2018-12-27 17:34:16 +1100

    Cleaned up App.js of boilerplate markup. Initialized React Router. Created Container div component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import styled from "styled-components";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="app">
+            <Container />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 70vw;
+  margin: auto;
+  padding: 20px 0;
+
+  @media (max-width: 700px) {
+    width: 95vw;
+  }
+`;
