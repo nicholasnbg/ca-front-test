0e1c7e6e6a291ce63f18a6a639c327bd428429b5
Nick Gray <nicholasnbg@gmail.com>
2018-12-29 11:19:05 +1100

    Refactored for es6, and changed magic number for fontSizeFactor into constant with instructions commented

--- client/src/components/ResponseGauge.js
+++ client/src/components/ResponseGauge.js
@@ -3,25 +3,28 @@ import { Doughnut } from "react-chartjs-2";
 import { Chart } from "chart.js";
 import PropTypes from "prop-types";
 
-import { responseColors } from "./colors";
+import { responseColors, gaugeColors } from "./colors";
 
 //Code pulled from StackOverflow to allow text in center of gauge
-var originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
+const originalDoughnutDraw = Chart.controllers.doughnut.prototype.draw;
 Chart.helpers.extend(Chart.controllers.doughnut.prototype, {
   draw: function() {
     originalDoughnutDraw.apply(this, arguments);
 
-    var chart = this.chart;
-    var width = chart.chart.width,
+    // Change this up to make inner text smaller, change this down to make inner text larger
+    const fontSizeFactor = 114;
+
+    const chart = this.chart;
+    const width = chart.chart.width,
       height = chart.chart.height,
       ctx = chart.chart.ctx;
 
-    var fontSize = (height / 114).toFixed(2);
+    const fontSize = (height / fontSizeFactor).toFixed(2);
     ctx.font = fontSize + "em sans-serif";
     ctx.textBaseline = "middle";
 
     //Sets inner gauge text to response number + %
-    var text = chart.config.data.datasets[0].data[0] + "%",
+    const text = chart.config.data.datasets[0].data[0] + "%",
       textX = Math.round((width - ctx.measureText(text).width) / 2),
       textY = height / 2;
 
@@ -37,7 +40,7 @@ export default class ResponseGauge extends Component {
       datasets: [
         {
           data: [0, 0],
-          backgroundColor: ["#43A089", "#b5b5b5"]
+          backgroundColor: gaugeColors
         }
       ]
     },
