6742593455b34dc51960d443a230e1158206b680
Nick Gray <nicholasnbg@gmail.com>
2018-12-29 11:17:39 +1100

    Created function to return approapriate color for a response rating

--- client/src/components/colors.js
+++ client/src/components/colors.js
@@ -5,3 +5,10 @@ export const responseColors = [
   "#2db300",
   "#43A089"
 ];
+
+export const gaugeColors = ["#43A089", "#b5b5b5"];
+
+//Returns respective color from Response colors based on integer provided, int must be between 0 - 5
+export const getResponseColor = int => {
+  return responseColors[Math.floor(int === 5 ? 4.9 : int)];
+};
