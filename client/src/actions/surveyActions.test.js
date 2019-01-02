7b5dcab26946152e1eb8496719ae0eb5a2d65212
Nick Gray <nicholasnbg@gmail.com>
2019-01-02 11:26:07 +1100

    Minor semantic fix

--- client/src/actions/surveyActions.test.js
+++ client/src/actions/surveyActions.test.js
@@ -2,7 +2,7 @@ import { getSurvey, setSurveyLoading } from "./surveyActions";
 import { GET_SURVEY, SURVEY_LOADING } from "../actions/types";
 
 describe("Survey Actions", () => {
-  it("can return SURVEY_LOADING ACTION", () => {
+  it("returns SURVEY_LOADING ACTION", () => {
     expect(setSurveyLoading()).toEqual({
       type: SURVEY_LOADING
     });
